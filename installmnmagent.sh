ff7eaba5f741fbbddc266730616483bce014aefa
dev1 <dev1@hostingcontroller.com>
2025-08-30 16:58:06 +0500

    api-gateway-2

--- installmnmagent.sh
+++ installmnmagent.sh
@@ -4,7 +4,7 @@
 # --- Configuration ---
 # Set your target VM's IP address and root password here
 IP="49.12.39.156"   # <-- Replace with your target VM IP
-PASSWORD="dneuqRCvLgaf"   # <-- Replace with your target VM root password
+PASSWORD="s4Ww9Pmcp9sk"   # <-- Replace with your target VM root password
 
 
 # Install sshpass if not present
