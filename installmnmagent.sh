c2b03757b5f0fd760457d7a382c8a8cff634ae4c
dev1 <dev1@hostingcontroller.com>
2025-08-31 16:07:34 +0500

    service installation issue check

--- installmnmagent.sh
+++ installmnmagent.sh
@@ -4,7 +4,7 @@
 # --- Configuration ---
 # Set your target VM's IP address and root password here
 IP="49.12.39.156"   # <-- Replace with your target VM IP
-PASSWORD="9qhCpu3wiPNM"   # <-- Replace with your target VM root password
+PASSWORD="bcqJA4CbKXCq"   # <-- Replace with your target VM root password
 
 
 # Install sshpass if not present
