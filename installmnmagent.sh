b69eab4cf14383b1424109915f00ebb54b7d01d7
dev1 <dev1@hostingcontroller.com>
2025-08-31 15:04:54 +0500

    gateway-2-onboarding test1

--- installmnmagent.sh
+++ installmnmagent.sh
@@ -4,7 +4,7 @@
 # --- Configuration ---
 # Set your target VM's IP address and root password here
 IP="49.12.39.156"   # <-- Replace with your target VM IP
-PASSWORD="i7jt7KciTUrw"   # <-- Replace with your target VM root password
+PASSWORD="V39EnimFfC7L"   # <-- Replace with your target VM root password
 
 
 # Install sshpass if not present
